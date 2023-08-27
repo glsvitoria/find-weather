26beabbdbafa4a8369c1e3bef6320b84b7100cd4
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-08-27 13:39:27 -0300

    fix: hide environment variables

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    OPEN_WEATHER_KEY: "00ca6488212176303dda00f246a78c46",
-    GEOAPIFY_KEY: "1ccf08422edd4f68ac1f07a55cf2aa9a",
-    NEXT_PUBLIC_API_URL: "http://localhost:3000/api",
-    // NEXT_PUBLIC_API_URL: "https://glsvitoria-weather.vercel.app/api",
-  },
   async headers() {
     return [
       {
