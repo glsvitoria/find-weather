cfa50346f4f0b8eee75245ed6a5407a471d0455a
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-08-27 03:02:16 -0300

    fix: cors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	env: {
-		OPEN_WEATHER_KEY: '00ca6488212176303dda00f246a78c46',
-		GEOAPIFY_KEY: '1ccf08422edd4f68ac1f07a55cf2aa9a',
-		NEXT_PUBLIC_API_URL: 'http://localhost:3000/api',
-	},
-}
+  reactStrictMode: true,
+  env: {
+    OPEN_WEATHER_KEY: "00ca6488212176303dda00f246a78c46",
+    GEOAPIFY_KEY: "1ccf08422edd4f68ac1f07a55cf2aa9a",
+    NEXT_PUBLIC_API_URL: "https://glsvitoria-weather.vercel.app/api",
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://glsvitoria-weather.vercel.app/api",
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Access-Control-Allow-Origin": "https://glsvitoria-weather.vercel.app",
-  },
 });
